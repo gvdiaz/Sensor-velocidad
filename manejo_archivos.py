93b3bf16dbeacae0876f80d2f3d33a813ef854e4
Gustavo V. Diaz <gdiaz@veng.com.ar>
2020-06-22 19:56:30 -0300

    Modificación de código. Pude utilizar las funciones de fecha y hora y pude dirigir directorios.

--- manejo_archivos.py
+++ manejo_archivos.py
@@ -9,19 +9,35 @@ Objetivo: Escribir scrip de python que pueda:
 
 """
 
+from datetime import datetime
 import os
 
-print("Ingresar nombre directorio, crack")
-teclado = input()
+#print("Ingresar nombre directorio, crack")
+carpeta = 'Ejemplo'
 
-print('Ingresar nombre de archivo')
-nombre_archivo = input() + '.txt'
+#print('Ingresar nombre de archivo')
+nombre_archivo = 'archivito' + '.txt'
 
-print('Ahora ingresate lo que va a haber dentro del archivo de prueba')
-cuerpo = input()
+#print('Ahora ingresate lo que va a haber dentro del archivo de prueba')
+cuerpo_1 = 'Fecha'
+cuerpo_2 = 'Hora'
+
+if os.path.isdir(carpeta):
+    None
+else:
+    os.mkdir(carpeta)
 
 #print(teclado + '\\' + nombre_archivo)
 
-f = open (teclado + "/" + nombre_archivo,'wt')
-f.write(cuerpo)
+ahora = datetime.now()
+fecha = str(ahora.day) + '/' + str(ahora.month) + '/' + str(ahora.year)
+tiempo = str(ahora.hour) + ':' + str(ahora.minute) + ':' + str(ahora.second)
+
+#f = open ('/' + carpeta + "/" + nombre_archivo,'wt')
+f = open (carpeta + '/' + nombre_archivo,'wt')
+f.write(cuerpo_1 + '\n')
+f.write(fecha + '\n')
+f.write(cuerpo_2 + '\n')
+f.write(tiempo)
+
 f.close()
